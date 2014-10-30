083f4313c9c431f0a94deb82ef9f54a26135cb2d
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2014-10-30 16:13:34 +0100

    work with CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL_MODULE as well

--- drivers/video/mxc/mipi_dsi.h
+++ drivers/video/mxc/mipi_dsi.h
@@ -99,13 +99,13 @@ int mipi_dsi_pkt_read(struct mipi_dsi_info *mipi,
 int mipi_dsi_dcs_cmd(struct mipi_dsi_info *mipi,
 				u8 cmd, const u32 *param, int num);
 
-#ifdef CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL
+#if defined(CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL) || defined(CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL_MODULE)
 void mipid_hx8369_get_lcd_videomode(struct fb_videomode **mode, int *size,
 		struct mipi_lcd_config **data);
 int mipid_hx8369_lcd_setup(struct mipi_dsi_info *);
 #endif
 
-#ifndef CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL
+#if !defined(CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL) && !defined(CONFIG_FB_MXC_TRULY_WVGA_SYNC_PANEL_MODULE)
 #error "Please configure MIPI LCD panel, we cannot find one!"
 #endif
 
